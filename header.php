67786a3933cd97f05b45d7b590803270a2a49ed3
alif1295 <alif1295@hotmail.com>
2017-04-16 16:08:46 +0800

    this is header

---
+++ header.php
@@ -0,0 +1,7 @@
+ <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+<body>
+
+ <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/scripts.js"></script>
