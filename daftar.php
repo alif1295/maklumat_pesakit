db8b0742dbb203031e1754f15835383bf3efb063
alif1295 <alif1295@hotmail.com>
2017-04-16 15:56:14 +0800

    page daftar

---
+++ daftar.php
@@ -0,0 +1,64 @@
+<?php  include("header.php"); ?>
+
+<div class="col-md-3" >
+				</div>
+				<div class="col-md-6" >
+                
+           <h1><center>Daftar</center></h1>
+					<hr>
+					<div class="panel panel-primary">
+					 
+					  <div class="panel-body"> 
+					  <form role="form" method="post" action="./process/daftar.php" id="myform" name="myform">
+						<div class="form-group">
+							 
+							<label for="nama">
+						Nama :
+							</label>
+							<input class="form-control" name="nama" id="nama" type="text">
+						</div>
+						
+						<div  class="form-group" >
+    <label for="katalaluan">Katalaluan :</label>
+	<input class="form-control" name="katalaluan" id="katalaluan" type="password">
+    
+  </div>
+
+						 
+						<button type="submit" class="btn btn-success">
+							Daftar
+						</button>
+						<a href="index.php" class="btn btn-primary">
+							Kembali
+						</a>
+					</form>
+					
+					
+					  
+					  </div>
+					</div>
+	
+					
+				</div>     
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+    
+  </body>
+</html>
