749d544c63d15f33a2af5fbfff014415dfa6d702
alif1295 <alif1295@hotmail.com>
2017-04-16 16:51:58 +0800

    this for home page

---
+++ home.php
@@ -0,0 +1,34 @@
+<?php
+
+		session_start();
+		include "include/db.php";
+		
+?>
+   <?php  include("header.php"); ?>
+	<?php  include("menu.php"); ?>
+	
+			
+			<div class="alert alert-success">
+			  <center><strong>Selamat Datang !,<b> <?php echo $_SESSION['nama']; ?><b></strong></center>  
+			</div>  
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
