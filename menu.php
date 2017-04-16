a4c7aa6803d6847ec3e6194b54ca65b66a3066b0
alif1295 <alif1295@hotmail.com>
2017-04-16 17:06:07 +0800

    this is menu page

---
+++ menu.php
@@ -0,0 +1,36 @@
+<div class="row">
+		<div class="col-md-12">
+			
+			<nav class="navbar navbar-default  " role="navigation" style="background-color: #040759">
+				 
+				
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav">
+						<li >
+							<a href="home.php">Utama</a>
+						</li>
+						<li>
+									<a href="tambah_maklumat_pesakit.php">Tambah Maklumat Pesakit</a>
+								</li>
+								<li>
+									<a href="senarai_maklumat_pesakit.php">Senarai Maklumat Pesakit</a>
+								</li>				 
+						
+						
+								<li>
+									<a href="senarai_pengguna.php">Senarai Pengguna</a>
+							 
+						
+					</ul>
+					 
+					<ul class="nav navbar-nav navbar-right">
+						<li>
+							<a href="logout.php" onclick="return confirm('Anda Pasti?');">Keluar</a>
+						</li>
+						 
+					</ul>
+				</div>
+				
+			</nav>
+		</div>
+	</div>
